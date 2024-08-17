f24c7beeb53662cfaa08edc0b0439f9507d8d349
Animesh Srivastava <animeshsrivastava2003@gmail.com>
2024-08-17 18:17:48 +0530

    Config Keys

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,12 +10,7 @@ export default defineNuxtConfig({
       sessionCookie: false,
     },
     config: {
-      apiKey: "AIzaSyCX7pV6zgXX2EyY6GXRR2dOzwMvrqmq5Pc",
-      authDomain: "substore-1f80d.firebaseapp.com",
-      projectId: "substore-1f80d",
-      storageBucket: "substore-1f80d.appspot.com",
-      messagingSenderId: "829797065640",
-      appId: "1:829797065640:web:01195dd9eb23236b06739d"
+      //Firebase Config
     }
   },
 })
