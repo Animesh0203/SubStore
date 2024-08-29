8cce740e3d412c1871f3160268f3f4a18b6ee27c
Animesh Srivastava <98380129+Animesh0203@users.noreply.github.com>
2024-08-29 14:49:02 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,12 +10,14 @@ export default defineNuxtConfig({
       sessionCookie: false,
     },
     config: {
-      apiKey: "AIzaSyCX7pV6zgXX2EyY6GXRR2dOzwMvrqmq5Pc",
-      authDomain: "substore-1f80d.firebaseapp.com",
-      projectId: "substore-1f80d",
-      storageBucket: "substore-1f80d.appspot.com",
-      messagingSenderId: "829797065640",
-      appId: "1:829797065640:web:01195dd9eb23236b06739d"
+      public: {
+        apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID
+      }
     }
   },
 })
